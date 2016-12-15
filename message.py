f082a62411c979d600d7a83b1b16d366e1301ffc
GreatDrake <nikich_morozov@mail.ru>
2016-12-15 19:20:38 +0300

    Added Message class

---
+++ message.py
@@ -0,0 +1,16 @@
+import os
+from PyQt5.QtWidgets import QWidget, QMessageBox
+from PyQt5.QtGui import QIcon
+
+#Класс для вывода различный QMessageBox с сообщениями
+class Message(QWidget):
+    
+    @staticmethod
+    def errorMessage(cur_self, title, text):
+        box = QMessageBox(cur_self)
+        box.setText(text)
+        box.setWindowTitle(title)
+        box.setIcon(QMessageBox.Critical)
+        box.setWindowIcon(QIcon(os.path.join("Resources", "empt.ico")))
+        box.setStandardButtons(QMessageBox.Ok)
+        box.exec_()
